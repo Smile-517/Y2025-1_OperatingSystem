432e9346161a0a0b0732b0056169daf41c7167c4
Smile-517 <ch020517@gmail.com>
2025-04-02 17:59:43 +0900

    formatting

--- ps.c
+++ ps.c
@@ -2,17 +2,13 @@
 #include "stat.h"
 #include "user.h"
 
-int main(int argc, char** argv)
-{
-	unsigned int i;
-	int sum=0;
-	for (i=0; i< 1000000000; ++i)
-		sum += i;
-	printf(1, "%d\n", sum);
+int main(int argc, char** argv) {
+    unsigned int i;
+    int sum = 0;
+    for (i = 0; i < 1000000000; ++i) sum += i;
+    printf(1, "%d\n", sum);
 
-	//uncomment the following line when your ps system call is ready
-	//ps(); 
-	exit();
+    // uncomment the following line when your ps system call is ready
+    // ps();
+    exit();
 }
-
-
