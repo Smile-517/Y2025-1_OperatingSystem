a0dff1f0f8d6c0602f0b156301aa7e8805d3a583
Smile-517 <ch020517@gmail.com>
2025-04-02 18:30:58 +0900

    add mapping for yield()

--- syscall.c
+++ syscall.c
@@ -91,15 +91,35 @@ extern int sys_swapwrite(void);
 // New system calls
 extern int sys_sched_setattr(void);
 extern int sys_sched_getattr(void);
+extern int sys_yield(void);
 
 static int (*syscalls[])(void) = {
-    [SYS_fork] sys_fork,   [SYS_exit] sys_exit,         [SYS_wait] sys_wait,           [SYS_pipe] sys_pipe,
-    [SYS_read] sys_read,   [SYS_kill] sys_kill,         [SYS_exec] sys_exec,           [SYS_fstat] sys_fstat,
-    [SYS_chdir] sys_chdir, [SYS_dup] sys_dup,           [SYS_getpid] sys_getpid,       [SYS_sbrk] sys_sbrk,
-    [SYS_sleep] sys_sleep, [SYS_uptime] sys_uptime,     [SYS_open] sys_open,           [SYS_write] sys_write,
-    [SYS_mknod] sys_mknod, [SYS_unlink] sys_unlink,     [SYS_link] sys_link,           [SYS_mkdir] sys_mkdir,
-    [SYS_close] sys_close, [SYS_swapread] sys_swapread, [SYS_swapwrite] sys_swapwrite, [SYS_sched_setattr] sys_sched_setattr,
+    [SYS_fork] sys_fork,
+    [SYS_exit] sys_exit,
+    [SYS_wait] sys_wait,
+    [SYS_pipe] sys_pipe,
+    [SYS_read] sys_read,
+    [SYS_kill] sys_kill,
+    [SYS_exec] sys_exec,
+    [SYS_fstat] sys_fstat,
+    [SYS_chdir] sys_chdir,
+    [SYS_dup] sys_dup,
+    [SYS_getpid] sys_getpid,
+    [SYS_sbrk] sys_sbrk,
+    [SYS_sleep] sys_sleep,
+    [SYS_uptime] sys_uptime,
+    [SYS_open] sys_open,
+    [SYS_write] sys_write,
+    [SYS_mknod] sys_mknod,
+    [SYS_unlink] sys_unlink,
+    [SYS_link] sys_link,
+    [SYS_mkdir] sys_mkdir,
+    [SYS_close] sys_close,
+    [SYS_swapread] sys_swapread,
+    [SYS_swapwrite] sys_swapwrite,
+    [SYS_sched_setattr] sys_sched_setattr,
     [SYS_sched_getattr] sys_sched_getattr,
+    [SYS_yield] sys_yield,
 };
 
 void syscall(void) {
