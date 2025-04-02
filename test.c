44b8e19f5ca725f94c94f7a3dc1968d79a3bcf8c
Smile-517 <ch020517@gmail.com>
2025-04-02 18:29:46 +0900

    more test for setattr(), getattr(), yield()

--- test.c
+++ test.c
@@ -5,12 +5,32 @@
 #include "user.h"
 
 int main() {
-    printf(1, "sched_setattr(), return value: %d\n", sched_setattr(10, 10));
+    int* request_tickPB = malloc(sizeof(int));
+    int* weightPB = malloc(sizeof(int));
+    printf(1, "sched_getattr() before calling sched_setattr(), return value: %d\n",
+           sched_getattr(request_tickPB, weightPB));
+    printf(1, "request_tick: %d, weight: %d\n", *request_tickPB, *weightPB);
 
+    printf(1, "first sched_setattr(), return value: %d\n", sched_setattr(100, 3));
     int* request_tickP = malloc(sizeof(int));
     int* weightP = malloc(sizeof(int));
-    printf(1, "sched_getattr(), return value: %d\n", sched_getattr(request_tickP, weightP));
+    printf(1, "sched_getattr() after calling sched_setattr(), return value: %d\n",
+           sched_getattr(request_tickP, weightP));
     printf(1, "request_tick: %d, weight: %d\n", *request_tickP, *weightP);
 
-    return 0;
+    int pid;
+    int* request_tickP2 = malloc(sizeof(int));
+    int* weightP2 = malloc(sizeof(int));
+    if ((pid = fork()) == 0) {  // I am child
+        yield();
+        printf(1, "sched_getattr() at child, return value: %d\n", sched_getattr(request_tickP2, weightP2));
+        printf(1, "request_tick: %d, weight: %d\n", *request_tickP2, *weightP2);
+        printf(1, "child finished\n");
+    } else {  // I am parent
+        // yield();
+        printf(1, "parent finished\n");
+    }
+
+    exit();
+    return 0;  // not reachable
 }
